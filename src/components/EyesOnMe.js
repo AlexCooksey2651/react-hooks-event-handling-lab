984ce6568ceb2cd8b6ac496f33474d7fb14568a1
Alex Cooksey <aecooksey2651@gmail.com>
2022-01-07 11:40:12 -0500

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function printGood() {
+        console.log('Good!')
+    }
+    function printEyesOnMe() {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus={printGood} onBlur={printEyesOnMe}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
